9cbf6156f5c25fbe73610ddb33358d1f603f0c8a
Preethi <141437665+Preethi-Puttambaku@users.noreply.github.com>
2026-04-01 14:05:36 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,13 @@
+a = 3777 #id = memory location 
+b = 3777
+c = 30
+d = a+b+c
+print(d)
+print(id(a),id(b),id(c))
+
+#print the sum of two numbers
+
+x = 21
+y = 25
+z = x+y
+print("The sum of two numbers:",z)
